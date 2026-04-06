aff35dc864579b82286875146c2fd15041e5a0b3
Jayant Jaiswal <jayantjaiswal886@gmail.com>
2026-04-06 21:26:07 +0530

    Add files via upload

---
+++ Roman to Integer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int romanToInt(string s) {
+    unordered_map<char, int> roman = {
+        {'I',1}, {'V',5}, {'X',10},
+        {'L',50}, {'C',100}, {'D',500}, {'M',1000}
+    };
+
+    int total = 0;
+
+    for(int i = 0; i < s.length(); i++) {
+        if (i < s.length() - 1 && roman[s[i]] < roman[s[i+1]]) {
+            total -= roman[s[i]];
+        } else {
+            total += roman[s[i]];
+        }
+    }
+
+    return total;
+}
+
+int main() {
+    string str;
+    cout<<"enter the roman number ";
+    cin>>str;
+    cout <<" roman number in integer is :" <<romanToInt(str);
+    return 0;
+}
